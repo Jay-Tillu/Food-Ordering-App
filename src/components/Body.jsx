fa983be0bdf60d7a07b1eaaed1b6736f2eeaa532
Jay Tillu <>
2024-09-17 23:10:06 +0530

    filter btn added

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -5,7 +5,11 @@ import resList from "../utils/mockData";
 function Body() {
   return (
     <div className="body">
-      <div className="search">Search</div>
+      <div className="filter">
+        <button className="filter-btn">
+            Top Rated Restaurant            
+        </button>
+      </div>
       <div className="res-container">
         {resList.map((restaurant) => (
           <RestaurantCard resData={restaurant} key={restaurant.id} />
